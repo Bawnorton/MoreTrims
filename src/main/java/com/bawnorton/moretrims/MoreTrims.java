82d62e83d14c78c1cb8f5c5a09c8cf8e91df9b1a
Benjamin-Norton <bawnorton@me.com>
2023-03-15 22:25:13 -0700

    add full automation to python script before port to java

--- src/main/java/com/bawnorton/moretrims/MoreTrims.java
+++ src/main/java/com/bawnorton/moretrims/MoreTrims.java
@@ -6,7 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MoreTrims implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("moretrims");
+	public static final String MOD_ID = "moretrims";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
